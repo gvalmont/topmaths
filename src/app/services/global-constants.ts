9416cdd4471bfab5ed7b4bc872f0daa2c425dfa9
Guillaume Valmont <guillaume.valmont@pm.me>
2022-01-19 18:30:42 +0400

    chore: Changement de l'adresse de l'api

--- src/app/services/global-constants.ts
+++ src/app/services/global-constants.ts
@@ -1,6 +1,6 @@
 export class GlobalConstants {
   public static origine: string = 'https://topmaths.fr/'
-  public static apiUrl: string = 'https://topmaths.fr/' + "api/"
+  public static apiUrl: string = 'https://api.topmaths.fr/'
   public static derniereSequence6e: number = 0
   public static derniereSequence5e: number = 5
   public static derniereSequence4e: number = 6
