e47d3a8455f65dcf0f42bb28f8055cba64488c34
Guillaume Valmont <guillaume.valmont@pm.me>
2022-03-15 15:02:54 +0400

    Mise à jour des dernières séquences

--- src/app/services/global-constants.ts
+++ src/app/services/global-constants.ts
@@ -2,7 +2,7 @@ export class GlobalConstants {
   public static origine: string = 'https://topmaths.fr/'
   public static apiUrl: string = 'https://api.topmaths.fr/'
   public static derniereSequence6e: number = 0
-  public static derniereSequence5e: number = 6
-  public static derniereSequence4e: number = 8
-  public static derniereSequence3e: number = 10
+  public static derniereSequence5e: number = 8
+  public static derniereSequence4e: number = 9
+  public static derniereSequence3e: number = 14
 }
