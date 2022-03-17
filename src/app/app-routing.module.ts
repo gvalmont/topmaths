bc91721dbfc0537606236e60eb48f30344fd16c7
Guillaume Valmont <guillaume.valmont@pm.me>
2022-03-17 14:49:08 +0400

    feat: Certaines fonctionnalités sont maintenant inaccessibles pendant les cours

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { JeuxComponent } from './jeux/jeux.component';
 import { LoginComponent } from './login/login.component';
 import { ProfilComponent } from './profil/profil.component';
 import { AuthguardGuard } from './services/auth.guard';
+import { TimeguardGuard } from './services/time.guard';
 import { ClassementComponent } from './classement/classement.component';
 import { SPS1Component } from './sequencesParticulieres/sps1/sps1.component';
 import { TropheesComponent } from './trophees/trophees.component';
@@ -30,12 +31,12 @@ const routes: Routes = [
   { path: 'sequences', redirectTo: 'sequences/tout' },
   { path: 'sequence/SPS1', component: SPS1Component, data: {title: 'topmaths.fr - Programmation'} },
   { path: 'sequence/:ref', component: SequenceComponent },
-  { path: 'trophees/:ref', component: TropheesComponent, data: {title: 'topmaths.fr - Trophées'} },
-  { path: 'jeux', component: JeuxComponent, data: {title: 'topmaths.fr - Jeux'} },
+  { path: 'trophees/:ref', component: TropheesComponent, canActivate: [TimeguardGuard], data: {title: 'topmaths.fr - Trophées'} },
+  { path: 'jeux', component: JeuxComponent, canActivate: [TimeguardGuard], data: {title: 'topmaths.fr - Jeux'} },
   { path: 'login', component: LoginComponent, data: {title: 'topmaths.fr - Login'} },
-  { path: 'profil/avatar', component: AvatarComponent, data: {title: 'topmaths.fr - Avatar'}},
+  { path: 'profil/avatar', component: AvatarComponent, canActivate: [TimeguardGuard], data: {title: 'topmaths.fr - Avatar'}},
   { path: 'profil', component: ProfilComponent, canActivate: [AuthguardGuard], data: {title: 'topmaths.fr - Profil'} },
-  { path: 'classement/:categorie', component: ClassementComponent, data: {title: 'topmaths.fr - Classement'} },
+  { path: 'classement/:categorie', component: ClassementComponent, canActivate: [TimeguardGuard], data: {title: 'topmaths.fr - Classement'} },
   { path: 'classement', redirectTo: 'classement/individuel' },
   { path: 'revisions', component: RevisionsComponent, data: {title: 'topmaths.fr - Révisions'} },
   { path: 'exercices-au-hasard/:type', component: ExercicesAuHasardComponent, data: {title: 'topmaths.fr - Exercices au hasard'} },
@@ -43,8 +44,8 @@ const routes: Routes = [
   { path: 'competitions/:action/:type', component: CompetitionsComponent, canActivate: [AuthguardGuard] },
   { path: 'competitions/:action', component: CompetitionsComponent, canActivate: [AuthguardGuard] },
   { path: 'competitions', component: CompetitionsComponent, data: {title: 'topmaths.fr - Compétitions'} },
-  { path: 'team/admin/:ref', component: EquipeModificationComponent, canActivate: [AuthguardGuard], data: {title: 'topmaths.fr - Modification de l\'équipe'}},
-  { path: 'team/:teamName', component: EquipePageComponent},
+  { path: 'team/admin/:ref', component: EquipeModificationComponent, canActivate: [AuthguardGuard, TimeguardGuard], data: {title: 'topmaths.fr - Modification de l\'équipe'}},
+  { path: 'team/:teamName', canActivate: [TimeguardGuard], component: EquipePageComponent},
   { path: '', component: AccueilComponent },
   { path: ':ref', component: ObjectifComponent },
   { path: '**', component: AccueilComponent }
