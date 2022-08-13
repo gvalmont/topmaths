9df1ec0c6c38d15e30f429de06a470be0c1c6112
Guillaume Valmont <guillaume.valmont@pm.me>
2022-08-13 22:07:15 +0200

    feat: L'impression imprime par défaut 3x plus d'exercices

--- src/app/mini-components/bouton-imprimer/bouton-imprimer.component.ts
+++ src/app/mini-components/bouton-imprimer/bouton-imprimer.component.ts
@@ -8,10 +8,26 @@ import { Component, Input } from '@angular/core'
 export class BoutonImprimerComponent {
   @Input() lien: string
   @Input() size: number
+  COEF_MULTIPLICATEUR_IMPRESSION_ENTRAINEMENT = 3
 
   constructor () {
     this.lien = ''
     this.size = 2
   }
 
+  lienTraitePourImpression () {
+    const parts = this.lien.replace('v=e','v=latex').split(',n=')
+    let lien = ''
+    for (let i = 0; i < parts.length; i++) {
+      if (i === 0) {
+        lien = parts[i]
+      } else {
+        lien += (parseInt(parts[i].slice(0, 1)) * this.COEF_MULTIPLICATEUR_IMPRESSION_ENTRAINEMENT).toString() + parts[i].slice(1)
+      }
+      if (i < parts.length - 1) lien += ',n='
+    }
+    console.log(this.lien)
+    console.log(lien)
+    return lien
+  }
 }
