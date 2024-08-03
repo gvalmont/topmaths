9699d0037b4e79f423ed54a9532c86a64871dd94
Guillaume Valmont <guillaume.valmont@pm.me>
2024-08-03 17:59:59 +0400

    Ajout de grade-container et stylisation des titres en dark mode

--- src/topmaths/components/Unit/UnitRegular.svelte
+++ src/topmaths/components/Unit/UnitRegular.svelte
@@ -23,8 +23,13 @@
   <title>Séquence {unit.number} : {unit.title}</title>
 </svelte:head>
 
-<h1 class="font-semibold p-4 title is-{unit.grade} rounded-t-4xl md:rounded-t-5xl
-  text-2xl md:text-5xl"
+<div class="grade-container is-{unit.grade}
+  rounded-4xl md:rounded-5xl"
+>
+
+<h1 class="font-semibold p-4 title
+  text-2xl md:text-5xl
+  rounded-t-4xl md:rounded-t-5xl"
 >
   Séquence {unit.reference.slice(3)} :<br />{unit.title}
 </h1>
@@ -147,9 +152,7 @@
 {/if}
 <div
   id="divEvaluation"
-  class="is-{unit.grade} {unit.downloadLinks.lessonLink || unit.downloadLinks.lessonSummaryLink || unit.downloadLinks.missionLink || ($isPersonalMode && unit.downloadLinks.lessonPlanLink)
-    ? ''
-    : ' is-end'}"
+  class="is-{unit.grade}"
 >
   <h2 id="Evaluation" class="subtitle text-xl md:text-3xl p-3 is-{unit.grade}">Évaluation</h2>
   <div class="p-6  flex flex-col">
@@ -176,7 +179,7 @@
   </div>
 </div>
 {#if unit.downloadLinks.lessonLink || unit.downloadLinks.lessonSummaryLink || unit.downloadLinks.missionLink || ($isPersonalMode && unit.downloadLinks.lessonPlanLink) }
-  <div class="is-end is-{unit.grade}">
+  <div class="is-{unit.grade}">
     <h2 class="subtitle text-xl md:text-3xl p-3 is-{unit.grade}">Téléchargements</h2>
     <ul class="p-6 ">
       <DownloadLine
@@ -202,3 +205,4 @@
     </ul>
   </div>
 {/if}
+</div>
