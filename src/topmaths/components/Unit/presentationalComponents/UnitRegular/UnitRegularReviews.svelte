1710ee271c76da3cbc317e8de625475567dd946d
Guillaume Valmont <guillaume.valmont@pm.me>
2024-08-19 20:53:33 +0400

    Les lignes de révisions ne s'affichent plus s'il n'y a rien à réviser

--- src/topmaths/components/Unit/presentationalComponents/UnitRegular/UnitRegularReviews.svelte
+++ src/topmaths/components/Unit/presentationalComponents/UnitRegular/UnitRegularReviews.svelte
@@ -13,20 +13,26 @@
 </h2>
 <div class="p-6 flex flex-col">
   {#each unit.objectives.map(objective => objective.lessonPlans).flat() as lessonPlan, i}
-  <div class="flex flex-row justify-center">
-    Séance {i + 1} :
-    <ExercisesButtons
-      class="ml-2"
-      exercisesLink= {lessonPlan.consolidationLink}
-    >
-      Consolidation
-    </ExercisesButtons>
-    <ExercisesButtons
-      class="ml-4"
-      exercisesLink= {lessonPlan.prerequisiteLink}
-    >
-      Prérequis
-    </ExercisesButtons>
-  </div>
+    {#if lessonPlan.consolidationLink !== '' || lessonPlan.prerequisiteLink !== ''}
+      <div class="flex flex-row justify-center">
+        Séance {i + 1} :
+        {#if lessonPlan.consolidationLink !== ''}
+          <ExercisesButtons
+            class="ml-2"
+            exercisesLink= {lessonPlan.consolidationLink}
+          >
+            Consolidation
+          </ExercisesButtons>
+        {/if}
+        {#if lessonPlan.prerequisiteLink !== ''}
+          <ExercisesButtons
+            class="ml-4"
+            exercisesLink= {lessonPlan.prerequisiteLink}
+          >
+            Prérequis
+          </ExercisesButtons>
+        {/if}
+      </div>
+    {/if}
   {/each}
 </div>
