aa776df718a7dc8839c7e78446aa03538dcffe23
Guillaume Valmont <guillaume.valmont@pm.me>
2024-08-27 19:03:43 +0400

    Le dark mode toggle n'apparaît plus sur le menu sur les petits écrans

--- src/topmaths/components/presentationalComponents/DarkModeToggle.svelte
+++ src/topmaths/components/presentationalComponents/DarkModeToggle.svelte
@@ -4,7 +4,8 @@
 </script>
 
 <label
-  class="swap swap-rotate fixed right-4 top-4 z-50
+  class="swap swap-rotate fixed right-4 z-50
+    top-12 md:top-4
     text-amber-500 hover:text-amber-400
     dark:text-amber-400 dark:hover:text-amber-300"
 >
