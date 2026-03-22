912cb1eb65d58b99511869fd002decdf22e4fea0
Guillaume Valmont <guillaume.valmont@ac-reunion.fr>
2026-03-22 21:58:18 +0400

    style: la modale de confirmation de lien copié est maintenant centrée

--- src/topmaths/components/presentationalComponents/InfoDialog.svelte
+++ src/topmaths/components/presentationalComponents/InfoDialog.svelte
@@ -1,6 +1,6 @@
 <dialog
   id="topmaths-info-dialog"
-  class="rounded-xl p-6 shadow-lg
+  class="rounded-xl p-6 shadow-lg m-auto
     text-coopmaths-corpus dark:text-coopmathsdark-corpus
     bg-coopmaths-canvas dark:bg-coopmathsdark-canvas"
 ></dialog>
