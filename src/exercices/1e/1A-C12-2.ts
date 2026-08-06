5132b674de4523d190be0ff3d55fa49cc7dac8d4
Stéphane Guyon <stephane.guyon@ac-montpelier.fr>
2026-08-06 14:13:31 +0200

    Create 1A-C12-2.ts

---
+++ src/exercices/1e/1A-C12-2.ts
@@ -0,0 +1,25 @@
+import AutoQ4AGt2026 from '../EAMPremiere/EAM-AGTechno-2026-Q4'
+
+export const uuid = '1c981'
+export const refs = {
+  'fr-fr': ['1A-C12-2'],
+  'fr-ch': [],
+}
+export const interactifReady = true
+export const interactifType = 'qcm'
+export const amcReady = 'true'
+export const amcType = 'qcmMono'
+export const titre = 'Convertir des degrés Celsius en degrés Fahrenheit'
+export const dateDePublication = '06/08/2026'
+
+/**
+ * @author Jean-Claude Lhote, clone de Stéphane Guyon
+ * Clone de EAM-AGTechno-2026-Q4 en version exclusivement aléatoire.
+ */
+export default class ConvertirCelsiusEnFahrenheit extends AutoQ4AGt2026 {
+  constructor() {
+    super()
+    this.besoinFormulaireCaseACocher = false
+    this.versionAleatoire()
+  }
+}
