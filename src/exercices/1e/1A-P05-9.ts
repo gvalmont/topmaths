3a55eb1b7e11a5bf9c6105a130db4a141e01aeda
Stéphane Guyon <stephane.guyon@ac-montpelier.fr>
2026-08-08 11:06:08 +0200

    Update 1A-P05-9.ts

--- src/exercices/1e/1A-P05-9.ts
+++ src/exercices/1e/1A-P05-9.ts
@@ -46,19 +46,19 @@ export default class TraduirePhraseParProbabilite extends ExerciceQcmA {
     let distracteurs: string[]
 
     if (typeProbabilite === 'conditionnelle') {
-      donnee = `$${pourcentage} \\%$ des ${situation.population} qui ${situation.ePluriel} ${situation.fPluriel}.`
+      donnee = `$${pourcentage} \\,\\%$ des ${situation.population} qui ${situation.ePluriel} ${situation.fPluriel}.`
       bonneReponse = 'P_E(F)'
       distracteurs = ['P(E\\cap F)', 'P(F)', 'P_F(E)']
-      explication = `La phrase « $${pourcentage} \\%$ des ${situation.population} qui ${situation.ePluriel} ${situation.fPluriel} » indique que l'on se place parmi les ${situation.population} qui ${situation.ePluriel}, c'est-à-dire parmi ceux qui réalisent $E$. On cherche, dans cette population de référence, la proportion de ceux qui réalisent $F$.<br>
+      explication = `La phrase « $${pourcentage} \\,\\%$ des ${situation.population} qui ${situation.ePluriel} ${situation.fPluriel} » indique que l'on se place parmi les ${situation.population} qui ${situation.ePluriel}, c'est-à-dire parmi ceux qui réalisent $E$. On cherche, dans cette population de référence, la proportion de ceux qui réalisent $F$.<br>
       Il s'agit donc de la probabilité conditionnelle $P_E(F)$.`
     } else if (typeProbabilite === 'intersection') {
-      donnee = `$${pourcentage} \\%$ des ${situation.population} ${situation.ePluriel} et ${situation.fPluriel}.`
+      donnee = `$${pourcentage} \\,\\%$ des ${situation.population} ${situation.ePluriel} et ${situation.fPluriel}.`
       bonneReponse = 'P(E\\cap F)'
       distracteurs = ['P_E(F)', 'P(F)', 'P(E\\cup F)']
       explication = `Les individus considérés réalisent à la fois $E$ et $F$. Cela correspond à l'intersection $E\\cap F$.<br>
       Il s'agit donc de la probabilité $P(E\\cap F)$.`
     } else {
-      donnee = `$${pourcentage} \\%$ de l'ensemble des ${situation.population} ${situation.fPluriel}.`
+      donnee = `$${pourcentage} \\,\\%$ de l'ensemble des ${situation.population} ${situation.fPluriel}.`
       bonneReponse = 'P(F)'
       distracteurs = ['P_E(F)', 'P(E\\cap F)', 'P(\\overline{E}\\cap F)']
       explication = `La proportion est donnée parmi l'ensemble des ${situation.population}, sans condition supplémentaire. Elle correspond donc à une probabilité totale.`
