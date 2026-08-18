b48752dcc42de1ea3961f7c05528c7b573a96daf
Stéphane Guyon <stephane.guyon@ac-montpelier.fr>
2026-08-18 21:44:48 +0200

    éclatement can2F3-04

---
+++ src/exercices/can/2e/can2F3-23.ts
@@ -0,0 +1,20 @@
+import ResoudreEquationsFonctionDeReference2 from './can2F3-04_old'
+
+export const titre = 'Résoudre une équation b − 1/x = c'
+export const interactifReady = true
+export const interactifType = 'qcm'
+export const dateDePublication = '18/08/2026'
+export const uuid = '6dfa9'
+export const refs = { 'fr-fr': ['can2F3-23'], 'fr-ch': [] }
+
+/**
+ * Modèle d'exercice très simple pour la course aux nombres
+ * @author Gilles Mora et Stéphane Guyon
+ */
+export default class EquationBMoinsInverse extends ResoudreEquationsFonctionDeReference2 {
+  constructor() {
+    super()
+    this.typeQuestionFixe = 6
+    this.besoinFormulaireNumerique = false
+  }
+}
