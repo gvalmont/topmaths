183841e7b56c4c0b7538042b989598213002d85d
Arnaud Meistermann <arnaud.meistermann@ac-montpellier.fr>
2026-08-18 22:18:55 +0000

    Arno modifie 3l13 1

--- src/exercices/2e/2N51-5.ts
+++ src/exercices/2e/2N51-5.ts
@@ -3,9 +3,14 @@ export const titre =
   'Résoudre une équation du premier degré (utilisant la distributivité)'
 export const interactifReady = true
 export const interactifType = 'mathLive'
-export const uuid = '3b3d9'
+export const uuid = '3ad01'
 export const refs = {
   'fr-fr': ['2N51-5', 'BP2RES30'],
   'fr-ch': ['NR'],
 }
-export default class ExerciceEquation1Tiret22nde extends ExerciceEquation1Tiret2 {}
+export default class ExerciceEquation1Tiret22nde extends ExerciceEquation1Tiret2 {
+  constructor() {
+    super(2)
+    this.niveau = 2
+  }
+}
