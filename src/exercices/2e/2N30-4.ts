2021b047a43a52234ae3c7152dd6ca755583db82
Stephane Guyon <stephane.guyon@ac-montpellier.fr>
2026-08-25 20:24:56 +0000

    MAJ 2N30-4 modifié

--- src/exercices/2e/2N30-4.ts
+++ src/exercices/2e/2N30-4.ts
@@ -1,174 +1,30 @@
-import {
-  addMultiMathfield,
-  type DataOptionsMultiMathfield,
-} from '../../lib/customElements/MultiMathfield'
-import { KeyboardType } from '../../lib/interactif/claviers/keyboard'
-import { toutAUnPoint } from '../../lib/interactif/fonctionsBaremes'
-import { handleAnswers } from '../../lib/interactif/gestionInteractif'
-import { choice, shuffle } from '../../lib/outils/arrayOutils'
-import { miseEnEvidence } from '../../lib/outils/embellissements'
-import { lettreMinusculeDepuisChiffre, sp } from '../../lib/outils/outilString'
-import { texNombre } from '../../lib/outils/texNombre'
-import type { ValeurNames } from '../../lib/types'
-import { fraction } from '../../modules/fractions'
-import { listeQuestionsToContenu, randint } from '../../modules/outils'
-import Exercice from '../Exercice'
-
-export const uuid = 'abdef'
-export const dateDePublication = '19/08/2026'
-export const titre = 'Comparer des nombres à $1$'
+import EncadrerFractionEntre2Entiers from '../6e/6N3I-1'
+export const titre = 'Encadrer une fraction entre deux nombres entiers'
 export const interactifReady = true
+export const interactifType = 'mathLive'
+export const amcReady = true
+export const amcType = 'AMCHybride'
+/**
+ * Clone de 6N20-1 pour les 2nde
+ *
+ * @author Jean-claude Lhote
+ */
+export const dateDeModifImportante = '14/05/2023' // ajout d'un paramètre pour choisir les dénominateurs
+export const uuid = 'd309b'
+
 export const refs = {
   'fr-fr': ['2N30-4'],
-  'fr-ch': [],
+  'fr-ch': ['9NO3C-3'],
 }
-
-/**
- * @author Jean-Claude Lhote
- * Comparer des nombres à un
- */
-
-const fraction6Chiffres = () => {
-  const num = randint(1, 8) * 100 + randint(0, 9) * 10 + randint(0, 9)
-  const delta = choice([-2, -1, 1, 2])
-  const den = num + delta
-  return { fraction: fraction(num, den) }
-}
-const fraction8Chiffres = () => {
-  const num =
-    randint(1, 8) * 1000 +
-    randint(0, 9) * 100 +
-    randint(0, 9) * 10 +
-    randint(0, 9)
-  const delta = choice([-2, -1, 1, 2])
-  const den = num + delta
-  return { fraction: fraction(num, den) }
-}
-const radicalSimple = () => {
-  const radical = choice([
-    randint(1, 99) / 100,
-    randint(1, 9) / 10,
-    randint(1, 9),
-  ])
-  return { radical }
-}
-
-const quotientDecimal = () => {
-  const expoNum = randint(-3, -1)
-  const expoDen = randint(-3, -1)
-  const num = randint(1, 9) * 10 ** expoNum
-  const den = randint(1, 9) * 10 ** expoDen
-  return { quotient: { num, den } }
-}
-
-const calcul = () => {
-  const typeCalcul = choice([1, 2])
-  switch (typeCalcul) {
-    case 1: {
-      const a = randint(1, 3)
-      const factor = randint(1, 3)
-      const b = randint(3, 6) * factor
-      const c = randint(-4, -2)
-      const d = b / factor - c
-      const expression = `${a} - \\dfrac{${b}}{${c}+${d}}`
-      const value = a - b / (c + d)
-      return { expression, value }
-    }
-    case 2:
-    default: {
-      const b = randint(5, 9)
-      const a = b + randint(1, 3)
-      const c = randint(1, 3)
-      const d = randint(2, 3, c)
-      const expression = `\\dfrac{${a}+${b}}{\\dfrac{${c}}{${d}}}`
-      const value = (a + b) / (c / d)
-      return { expression, value }
-    }
-  }
-}
-
-const generateurs = [
-  fraction6Chiffres,
-  fraction8Chiffres,
-  radicalSimple,
-  quotientDecimal,
-  calcul,
-]
-
-export default class ComparerDesNombresAUn extends Exercice {
+export default class EncadrerFractionEntre2Entiers2nde extends EncadrerFractionEntre2Entiers {
   constructor() {
     super()
-    this.spacing = 1
-    this.spacingCorr = 3
-    this.nbQuestions = 1
-    this.nbQuestionsModifiable = false
-    this.consigne =
-      "Pour chacun des nombres suivants, indiquez s'il est strictement inférieur à $1$"
-  }
-  nouvelleVersion() {
-    const fonctionsGeneratrices = shuffle(generateurs)
-    let templates = ``
-    let indexChamp = 1
-    const dataOptions: DataOptionsMultiMathfield = {}
-    const reponses: Record<string, unknown> = { bareme: toutAUnPoint }
-    let texteCorr = ''
-    for (const generateur of fonctionsGeneratrices) {
-      const nombre = generateur()
-      if ('fraction' in nombre) {
-        templates += `${lettreMinusculeDepuisChiffre(indexChamp)}. $${nombre.fraction.texFraction}$ %{champ${indexChamp}}${sp(10)}`
-        texteCorr += `${lettreMinusculeDepuisChiffre(indexChamp)}. $${nombre.fraction.texFraction} ${nombre.fraction.num < nombre.fraction.den ? miseEnEvidence('<') : nombre.fraction.num > nombre.fraction.den ? miseEnEvidence('>') : miseEnEvidence('=')} 1$ car le numérateur est ${nombre.fraction.num < nombre.fraction.den ? 'inférieur' : nombre.fraction.num > nombre.fraction.den ? 'supérieur' : 'égal'} au dénominateur.<br>`
-        if (nombre.fraction.num < nombre.fraction.den) {
-          reponses[`champ${indexChamp}`] = { value: '<' }
-        } else if (nombre.fraction.num > nombre.fraction.den) {
-          reponses[`champ${indexChamp}`] = { value: '>' }
-        } else {
-          reponses[`champ${indexChamp}`] = { value: '=' }
-        }
-      } else if ('radical' in nombre) {
-        templates += `${lettreMinusculeDepuisChiffre(indexChamp)}. $ \\sqrt{${texNombre(nombre.radical, 3)}}$ %{champ${indexChamp}}${sp(10)}`
-        texteCorr += `${lettreMinusculeDepuisChiffre(indexChamp)}. $ \\sqrt{${texNombre(nombre.radical, 3)}} ${nombre.radical < 1 ? miseEnEvidence('<') : nombre.radical > 1 ? miseEnEvidence('>') : miseEnEvidence('=')} 1$ car la fonction racine carrée est croissante, donc comme $${texNombre(nombre.radical, 3)} ${nombre.radical < 1 ? miseEnEvidence('<') : nombre.radical > 1 ? miseEnEvidence('>') : miseEnEvidence('=')} 1$ alors $ \\sqrt{${texNombre(nombre.radical, 3)}} ${nombre.radical < 1 ? miseEnEvidence('<') : nombre.radical > 1 ? miseEnEvidence('>') : miseEnEvidence('=')} \\sqrt{1}$.<br>`
-        if (nombre.radical < 1) {
-          reponses[`champ${indexChamp}`] = { value: '<' }
-        } else if (nombre.radical > 1) {
-          reponses[`champ${indexChamp}`] = { value: '>' }
-        } else {
-          reponses[`champ${indexChamp}`] = { value: '=' }
-        }
-      } else if ('quotient' in nombre) {
-        templates += `${lettreMinusculeDepuisChiffre(indexChamp)}. $\\dfrac{${texNombre(nombre.quotient.num, 3)}}{${texNombre(nombre.quotient.den, 3)}}$ %{champ${indexChamp}}${sp(10)}`
-        texteCorr += `${lettreMinusculeDepuisChiffre(indexChamp)}. $\\dfrac{${texNombre(nombre.quotient.num, 3)}}{${texNombre(nombre.quotient.den, 3)}} ${nombre.quotient.num < nombre.quotient.den ? miseEnEvidence('<') : nombre.quotient.num > nombre.quotient.den ? miseEnEvidence('>') : miseEnEvidence('=')} 1$ car le numérateur est ${nombre.quotient.num < nombre.quotient.den ? 'inférieur' : nombre.quotient.num > nombre.quotient.den ? 'supérieur' : 'égal'} au dénominateur.<br>`
-        if (nombre.quotient.num < nombre.quotient.den) {
-          reponses[`champ${indexChamp}`] = { value: '<' }
-        } else if (nombre.quotient.num > nombre.quotient.den) {
-          reponses[`champ${indexChamp}`] = { value: '>' }
-        } else {
-          reponses[`champ${indexChamp}`] = { value: '=' }
-        }
-      } else if ('expression' in nombre) {
-        templates += `${lettreMinusculeDepuisChiffre(indexChamp)}. $${nombre.expression}$ %{champ${indexChamp}}${sp(10)}`
-        texteCorr += `${lettreMinusculeDepuisChiffre(indexChamp)}. $${nombre.expression} ${nombre.value < 1 ? miseEnEvidence('<') : nombre.value > 1 ? miseEnEvidence('>') : miseEnEvidence('=')} 1$ car le calcul donne $${texNombre(nombre.value, 3)}$ comme résultat.<br>`
-        if (nombre.value < 1) {
-          reponses[`champ${indexChamp}`] = { value: '<' }
-        } else if (nombre.value > 1) {
-          reponses[`champ${indexChamp}`] = { value: '>' }
-        } else {
-          reponses[`champ${indexChamp}`] = { value: '=' }
-        }
-      }
-      dataOptions[`champ${indexChamp}` as ValeurNames] = {
-        keyboard: KeyboardType.clavierCompare,
-        minWidth: 15,
-        texteApres: '$1$',
-      }
-      indexChamp++
-    }
-
-    this.listeQuestions[0] = addMultiMathfield(this, 0, {
-      dataTemplate: templates,
-      dataOptions,
-    })
-    handleAnswers(this, 0, reponses, { formatInteractif: 'multi-mathfield' })
-    this.listeCorrections[0] = texteCorr
-    listeQuestionsToContenu(this)
+    this.lycee = true
+    this.sup = false
+    this.sup2 = '10'
+    this.besoinFormulaire2Texte = [
+      'Dénominateurs à choisir',
+      'Nombres séparés par des tirets :\nDe 2 à 9 pour les dénominateurs correspondants\n10 Mélange',
+    ]
   }
 }
