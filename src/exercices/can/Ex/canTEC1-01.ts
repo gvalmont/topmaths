a3273a9b190c268f01a977745b63006bd82c7ead
Stéphane Guyon <stephane.guyon@ac-montpelier.fr>
2026-08-30 09:34:57 +0200

    CanTEC1-2

--- src/exercices/can/Ex/canTEC1-01.ts
+++ src/exercices/can/Ex/canTEC1-01.ts
@@ -1,5 +1,9 @@
 import { amcConvert } from '../../../lib/amc/amcBuilders'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
 import { Complexe } from '../../../lib/mathFonctions/Complexe'
+import { rienSi1 } from '../../../lib/outils/ecritures'
+import { miseEnEvidence } from '../../../lib/outils/embellissements'
+import { texNombre } from '../../../lib/outils/texNombre'
 import { randint } from '../../../modules/outils'
 import ExerciceSimple from '../../ExerciceSimple'
 
@@ -12,7 +16,7 @@ export const dateDePublication = '26/10/2021'
 
 /**
  * Question de can : calcul de la somme de deux nombres complexes
- * @author Jean-claude Lhote
+ * @author Jean-claude Lhote, améliorations par Stéphane Guyon
 
 */
 export const uuid = '71292'
@@ -21,20 +25,45 @@ export const refs = {
   'fr-fr': ['canTEC1-01'],
   'fr-ch': ['3mNC-1'],
 }
+
+function ecritureComplexe(partieReelle: number, partieImaginaire: number) {
+  if (partieImaginaire === 0) return texNombre(partieReelle)
+  const partieImaginaireTex = `${rienSi1(partieImaginaire)}\\mathrm{i}`
+  if (partieReelle === 0) return partieImaginaireTex
+  return `${texNombre(partieReelle)}${partieImaginaire > 0 ? '+' : ''}${partieImaginaireTex}`
+}
+
 export default class SommeDeComplexes extends ExerciceSimple {
   constructor() {
     super()
 
     this.nbQuestions = 1
     this.typeExercice = 'simple'
+    this.formatChampTexte = {
+      clavierDeBase: KeyboardType.clavierDeBase,
+      complexes: KeyboardType.complexes,
+    }
   }
 
   nouvelleVersion() {
-    const z1 = new Complexe(randint(-5, 5), randint(-5, 5))
-    const z2 = new Complexe(randint(-5, 5), randint(-5, 5))
-    this.question = `On donne $~~a = ${z1.toString()}~~$ et $~~b = ${z2.toString()}$.<br>Calcule $a + b$.`
-    this.correction = `$${z1.toString()} + ${z2.toString()} = ${z1.add(z2).tex()}$`
-    this.reponse = z1.add(z2)
+    const a = randint(-5, 5, 0)
+    const b = randint(-5, 5, 0)
+    const c = randint(-5, 5, 0)
+    const d = randint(-5, 5, 0)
+    const z1 = new Complexe(a, b)
+    const z2 = new Complexe(c, d)
+    const somme = z1.add(z2)
+    const z1Tex = ecritureComplexe(a, b)
+    const z2Tex = ecritureComplexe(c, d)
+    const sommeTex = ecritureComplexe(a + c, b + d)
+    this.question = `Soit $z_1$ et $z_2$ deux nombres complexes définis par $z_1=${z1Tex}$ et $z_2=${z2Tex}$.<br>Calculer $z_1+z_2$.`
+    this.correction = `$\\begin{aligned}
+z_1+z_2
+&=(${z1Tex})+(${z2Tex})\\\\
+&=(${a}${c >= 0 ? '+' : ''}${c})+(${b}${d >= 0 ? '+' : ''}${d})\\mathrm{i}\\\\
+&=${miseEnEvidence(sommeTex)}.
+\\end{aligned}$`
+    this.reponse = somme
     this.autoCorrectionAMC[0] = {
       enonce: this.question,
       propositions: [
@@ -44,7 +73,7 @@ export default class SommeDeComplexes extends ExerciceSimple {
             {
               texte: this.correction,
               reponse: {
-                valeur: Number(z1.add(z2).re),
+                valeur: Number(somme.re),
                 param: {
                   digits: 2,
                   decimals: 0,
@@ -61,7 +90,7 @@ export default class SommeDeComplexes extends ExerciceSimple {
             {
               texte: '',
               reponse: {
-                valeur: Number(z1.add(z2).im),
+                valeur: Number(somme.im),
                 param: {
                   digits: 2,
                   decimals: 0,
