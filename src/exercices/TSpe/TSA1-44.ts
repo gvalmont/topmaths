d290b56136d49716ca3e7320807e43e3627c4ddb
Stephane Guyon <stephane.guyon@ac-montpellier.fr>
2026-08-31 18:55:45 +0000

    Sg tsa1 44

--- src/exercices/TSpe/TSA1-44.ts
+++ src/exercices/TSpe/TSA1-44.ts
@@ -1,100 +1,276 @@
-import { choice } from '../../lib/outils/arrayOutils'
-import { miseEnEvidence, texteCode } from '../../lib/outils/embellissements'
+import { createList } from '../../lib/format/lists'
+import { tableauDeVariation } from '../../lib/mathFonctions/etudeFonction'
+import { combinaisonListes } from '../../lib/outils/arrayOutils'
+import {
+  miseEnEvidence,
+  texteEnCouleurEtGras,
+} from '../../lib/outils/embellissements'
+import { reduireAxPlusB } from '../../lib/outils/ecritures'
 import { scriptPython } from '../../lib/outils/scriptPython'
 import { texNombre } from '../../lib/outils/texNombre'
-import { tableauColonneLigne } from '../../lib/2d/tableau'
-import { KeyboardType } from '../../lib/interactif/claviers/keyboard'
-import { randint } from '../../modules/outils'
-import ExerciceSimple from '../ExerciceSimple'
+import FractionEtendue from '../../modules/FractionEtendue'
+import { listeQuestionsToContenu } from '../../modules/outils'
+import Exercice from '../Exercice'
 
-export const titre = 'Déterminer la valeur renvoyée par un algorithme de seuil'
-export const dateDePublication = '29/08/2026'
+export const titre = 'Étudier une suite définie par une fonction homographique'
+export const dateDePublication = '31/08/2026'
 export const uuid = '87fb7'
-export const refs = {
-  'fr-fr': ['TSA1-44'],
-  'fr-ch': [],
-}
-export const interactifReady = true
+export const refs = { 'fr-fr': ['TSA1-44'], 'fr-ch': [] }
 
-/**
- * Exécuter une boucle Python de recherche de seuil et identifier la valeur
- * renvoyée.
- *
- * @author Stéphane Guyon
- */
-export default class ValeurRenvoyeeAlgorithmeSeuil extends ExerciceSimple {
-  nouvelleVersion(): void {
-    const lettre = choice(['u', 'v', 'w'])
-    const nomFonction = choice(['seuil', 'rangSeuil', 'premierRang'])
-    const u0 = randint(2, 10)
-    const coefficient = choice([2, 3])
-    const termeConstant = randint(2, 10)
-    const seuil = choice([500, 1000, 2000, 5000])
-    const coefficientTex = texNombre(coefficient)
-    const coefficientPython = String(coefficient)
-
-    const valeurs = [u0]
-    while (valeurs.at(-1)! <= seuil && valeurs.length < 100) {
-      valeurs.push(coefficient * valeurs.at(-1)! + termeConstant)
-    }
-    const rang = valeurs.length - 1
-    const valeurAvantSeuil = valeurs[rang - 1]
-    const valeurAuSeuil = valeurs[rang]
-
-    const programme = `def ${nomFonction}():
-    n = 0
-    ${lettre} = ${u0}
-    while ${lettre} <= ${seuil}:
-        n = n + 1
-        ${lettre} = ${coefficientPython}*${lettre} + ${termeConstant}
-    return n`
-
-    this.question = `On considère la suite $(${lettre}_n)$ définie sur $\\mathbb N$ par $${lettre}_0=${u0}$ et, pour tout entier naturel $n$, par :<br>
-$${lettre}_{n+1}=${coefficientTex}${lettre}_n+${termeConstant}$.<br><br>
-On définit la fonction Python suivante :<br>
-${scriptPython(programme, 6)}<br><br>
-On exécute cette fonction avec l'appel ${texteCode(`${nomFonction}()`)}.<br>
-Quelle valeur cet appel renvoie-t-il ?`
-
-    this.reponse = this.versionQcm ? `$${rang}$` : rang
-    this.distracteurs = [`$${rang - 1}$`, `$${rang + 1}$`, `$${rang + 2}$`]
-    this.versionQcmOptions = {
-      radio: true,
-      vertical: false,
-      dontKnow: this.sup4,
-    }
-
-    const rangs = valeurs.map((_, indice) => indice)
-    const tableauVariables = tableauColonneLigne(
-      [],
-      ['n', lettre],
-      [
-        ...rangs.map((valeur) => texNombre(valeur)),
-        ...valeurs.map((valeur) => texNombre(valeur)),
-      ],
-      1.2,
-    )
-
-    this.correction = `Les variables ${texteCode('n')} et ${texteCode(lettre)} sont initialisées avec le rang $0$ et le terme $${lettre}_0=${u0}$.<br><br>
-Les valeurs de $${lettre}$ sont calculées avec la relation de récurrence :<br>
-$${lettre}_1=${coefficientTex}\\times ${lettre}_0+${termeConstant}=${coefficientTex}\\times ${u0}+${termeConstant}=${valeurs[1]}$,<br>
-$${lettre}_2=${coefficientTex}\\times ${lettre}_1+${termeConstant}=${coefficientTex}\\times ${valeurs[1]}+${termeConstant}=${valeurs[2]}$.<br>
-Et ainsi de suite.<br><br>
-Le tableau d'exécution donne les valeurs exactes des variables à chaque test de la condition du ${texteCode('while')} :<br><br>
-${tableauVariables}<br><br>
-Au rang $${rang - 1}$, on a $${lettre}_{${rang - 1}}=${texNombre(valeurAvantSeuil)}\\leqslant ${seuil}$ : la boucle continue.<br>
-Au rang $${rang}$, on a $${lettre}_{${rang}}=${texNombre(valeurAuSeuil)}>${seuil}$ : la condition du ${texteCode('while')} devient fausse et la boucle s'arrête.<br>
-La fonction exécute alors ${texteCode('return n')} et renvoie donc $${miseEnEvidence(String(rang))}$.`
-  }
-
+/** Adaptation aléatoire du sujet 2 du baccalauréat 2026, Asie, exercice 1. */
+export default class SuiteEtFonctionHomographique extends Exercice {
   constructor() {
     super()
     this.nbQuestions = 1
-    this.besoinFormulaire4CaseACocher = ['Ajout de « Je ne sais pas »', false]
-    this.sup4 = false
     this.nbQuestionsModifiable = true
-    this.versionQcmDisponible = true
-    this.versionQcm = true
-    this.formatChampTexte = KeyboardType.clavierNumbers
+  }
+
+  nouvelleVersion(): void {
+    const configurations = combinaisonListes(
+      [
+        [2, 3],
+        [2, 4],
+        [3, 3],
+        [3, 4],
+        [4, 3],
+        [4, 4],
+        [5, 3],
+        [5, 4],
+      ],
+      this.nbQuestions,
+    )
+    for (let i = 0; i < this.nbQuestions; i++) {
+      const [p, precision] = configurations[i]
+      this.ajouteQuestion(p, precision)
+    }
+    listeQuestionsToContenu(this)
+  }
+
+  private ajouteQuestion(p: number, precision: number): void {
+    const h = 10 ** -precision
+    const rang = Math.ceil((1 / h - 1) / p)
+    const pole = new FractionEtendue(p + 1, p).texFraction
+    const limite = new FractionEtendue(p - 1, p).texFraction
+    const f0 = new FractionEtendue(p, p + 1).texFraction
+    const a = p - 1
+    const ax = a === 1 ? 'x' : `${a}x`
+    const numerateurF = reduireAxPlusB(a, -p)
+    const denominateurF = reduireAxPlusB(p, -(p + 1))
+    const numerateurSuite = reduireAxPlusB(a, -p, 'u_n')
+    const denominateurSuite = reduireAxPlusB(p, -(p + 1), 'u_n')
+    const f = `\\dfrac{${numerateurF}}{${denominateurF}}`
+    const hTex = texNombre(h, precision)
+    const programme = `def seuil(h):
+    n = 0
+    u = 0
+    while u < 1-h:
+        n = n+1
+        u = (${a}*u-${p})/(${p}*u-${p + 1})
+    return n`
+    const partieA = texteEnCouleurEtGras('Partie A', 'black')
+    const partieB = texteEnCouleurEtGras('Partie B', 'black')
+    const tableauVariations = tableauDeVariation({
+      tabInit: [
+        [
+          ['$x$', 2, 20],
+          ["$f'(x)$", 2, 30],
+          ['$f(x)$', 3, 30],
+        ],
+        ['$-\\infty$', 25, `$${pole}$`, 25],
+      ],
+      tabLines: [
+        ['Line', 30, '', 10, '+', 20, 'd', 10],
+        ['Var', 10, `-/$${limite}$`, 20, '+/$+\\infty$', 10],
+      ],
+      espcl: 8,
+      deltacl: 1.2,
+      lgt: 3.5,
+      scale: 1,
+    })
+    const tableauSigneDenominateur = tableauDeVariation({
+      tabInit: [
+        [
+          ['$x$', 2, 20],
+          [`$${p}x-${p + 1}$`, 2, 50],
+        ],
+        ['$-\\infty$', 25, `$${pole}$`, 25, '$+\\infty$', 25],
+      ],
+      tabLines: [['Line', 30, '', 10, '-', 20, 'z', 20, '+', 10]],
+      espcl: 6,
+      deltacl: 1,
+      lgt: 4,
+      scale: 1,
+    })
+
+    const questionsPartieA = createList({
+      style: 'nombres',
+      items: [
+        `Étudier les variations de la fonction $f$ sur son ensemble de définition.`,
+        `En déduire que, pour tout $x\\in[0\\,;\\,1]$, $f(x)\\in[0\\,;\\,1]$.`,
+      ],
+    })
+    const questionsPartieB = createList({
+      style: 'nombres',
+      items: [
+        `Démontrer que, pour tout $n\\in\\mathbb N$, $0\\leqslant u_n\\leqslant u_{n+1}\\leqslant1$.`,
+        `En déduire que $(u_n)$ converge, puis que sa limite est $1$.`,
+        `On donne la fonction Python suivante :<br>${scriptPython(programme, 7)}<br>
+        On saisit cette fonction dans la calculatrice. L'appel $\\mathtt{seuil(${hTex})}$ renvoie $${texNombre(rang)}$. Interpréter cette valeur.`,
+        createList({
+          style: 'alpha',
+          items: [
+            `Donner les quatre premiers termes sous forme de fractions irréductibles.`,
+            `Conjecturer l'expression de $u_n$ en fonction de $n$ et démontrer cette conjecture.`,
+          ],
+        }),
+      ],
+    })
+    const enonce = `${partieA}<br><br>On considère la fonction $f$ définie sur $\\left]-\\infty\\,;\\,${pole}\\right[$ par : \\[f(x)=${f}\\]
+    ${questionsPartieA}<br>
+    ${partieB}<br><br>On considère la suite $(u_n)$ définie par :
+    \\[\\begin{cases}u_0=0\\\\u_{n+1}=\\dfrac{${numerateurSuite}}{${denominateurSuite}},\\quad n\\in\\mathbb N\\end{cases}\\]
+    ${questionsPartieB}`
+
+    const termes = [0, 1, 2, 3].map((n) =>
+      n === 0
+        ? 'u_0=0'
+        : `u_{${n}}=${new FractionEtendue(p * n, p * n + 1).texFraction}`,
+    )
+    const general = `\\dfrac{${p}n}{${p}n+1}`
+    const seuil = texNombre(1 - h, precision)
+    const correctionsPartieA = createList({
+      style: 'nombres',
+      items: [
+        `$f$ est dérivable sur son domaine de définition.<br><br>
+        On reconnaît un quotient $f=\\dfrac{u}{v}$ avec
+        $u(x)=${numerateurF}$ et $v(x)=${denominateurF}$.<br><br>
+        Ainsi, $u'(x)=${a}$ et $v'(x)=${p}$, donc
+        \\[\\begin{aligned}
+        f'(x)&=\\dfrac{u'(x)v(x)-u(x)v'(x)}{v(x)^2}\\\\
+        &=\\dfrac{${a}(${p}x-${p + 1})-${p}(${ax}-${p})}{(${p}x-${p + 1})^2}\\\\
+        &=\\dfrac{1}{(${p}x-${p + 1})^2}.
+        \\end{aligned}\\]
+        Pour tout $x\\in\\left]-\\infty\\,;\\,${pole}\\right[$, $f'(x)>0$.<br><br>
+        La fonction $f$ est donc strictement croissante sur son domaine de définition.<br><br>
+        Pour $x\\neq 0$, on factorise le numérateur et le dénominateur par $x$ :
+        \\[f(x)=\\dfrac{x\\left(${a}-\\dfrac{${p}}x\\right)}{x\\left(${p}-\\dfrac{${p + 1}}x\\right)}
+        =\\dfrac{${a}-\\dfrac{${p}}x}{${p}-\\dfrac{${p + 1}}x}.\\]
+        Comme $\\displaystyle\\lim_{x\\to-\\infty}\\dfrac1x=0$, on obtient
+        $\\displaystyle\\lim_{x\\to-\\infty}f(x)=${limite}$.<br><br>
+        Étudions maintenant la limite à la borne supérieure du domaine.<br><br>
+        D'une part,
+        \\[\\lim_{x\\to${pole}^-}(${numerateurF})=${a}\\times${pole}-${p}=-\\frac1{${p}}<0.\\]
+        D'autre part,
+        \\[${p}x-${p + 1}>0\\iff x>${pole}.\\]
+        On en déduit le tableau de signes du dénominateur :<br><br>
+        ${tableauSigneDenominateur}<br><br>
+        Ainsi, lorsque $x$ tend vers $${pole}$ par valeurs inférieures, le numérateur tend vers $-\\frac1{${p}}<0$ et le dénominateur tend vers $0$ par valeurs négatives. Par quotient,
+        \\[\\lim_{x\\to${pole}^-}f(x)=+\\infty.\\]
+        On obtient finalement le tableau de variations suivant :<br><br>
+        ${tableauVariations}<br><br>
+        Ainsi, $${miseEnEvidence(`f\\text{ est strictement croissante sur }\\left]-\\infty\\,;\\,${pole}\\right[`)}$.`,
+        `Comme $x\\in[0\\,;\\,1]$, on a $0\\leqslant x\\leqslant 1$. Comme $f$ est croissante sur $[0\\,;\\,1]$, on obtient
+        \\[f(0)\\leqslant f(x)\\leqslant f(1).\\]
+        Or $f(0)=${f0}$ et $f(1)=1$. Ainsi,
+        \\[0\\leqslant ${f0}\\leqslant f(x)\\leqslant 1,\\]
+        donc $${miseEnEvidence(`f(x)\\in[0\\,;\\,1]`)}$.`,
+      ],
+    })
+    const correctionsPartieB = createList({
+      style: 'nombres',
+      items: [
+        `Nous allons procéder à un raisonnement par récurrence.<br>
+        Pour tout entier naturel $n$, on note $\\mathcal P_n$ la propriété :
+        \\[0\\leqslant u_n\\leqslant u_{n+1}\\leqslant 1.\\]<br>
+        ${texteEnCouleurEtGras('Initialisation :', 'black')}<br><br>
+        On a $u_0=0$ et
+        \\[u_1=f(u_0)=f(0)=${f0}.\\]
+        Comme $0\\leqslant ${f0}\\leqslant 1$, on a
+        \\[0\\leqslant u_0\\leqslant u_1\\leqslant 1.\\]
+        La propriété $\\mathcal P_0$ est donc vraie.<br><br>
+        ${texteEnCouleurEtGras('Hérédité :', 'black')}<br><br>
+        Soit $n\\in\\mathbb N$. Supposons que la propriété $\\mathcal P_n$ est vraie, c'est-à-dire :
+        \\[0\\leqslant u_n\\leqslant u_{n+1}\\leqslant 1.\\]
+        Montrons que la propriété $\\mathcal P_{n+1}$ est vraie, c'est-à-dire :
+        \\[0\\leqslant u_{n+1}\\leqslant u_{n+2}\\leqslant 1.\\]
+        On a :
+        \\[0\\leqslant u_n\\leqslant u_{n+1}\\leqslant 1.\\]
+        Comme $f$ est croissante sur $[0\\,;\\,1]$, les images et les antécédents sont rangés dans le même ordre :
+        \\[f(0)\\leqslant f(u_n)\\leqslant f(u_{n+1})\\leqslant f(1).\\]
+        Or $f(0)=${f0}$, $f(u_n)=u_{n+1}$, $f(u_{n+1})=u_{n+2}$ et $f(1)=1$.<br><br>
+        Ainsi,
+        \\[0\\leqslant ${f0}\\leqslant u_{n+1}\\leqslant u_{n+2}\\leqslant 1.\\]
+        La propriété $\\mathcal P_{n+1}$ est donc vraie. La propriété est héréditaire.<br><br>
+        ${texteEnCouleurEtGras('Conclusion :', 'black')}<br><br>
+        La propriété est vraie au rang $0$ et elle est héréditaire. Donc, par récurrence, pour tout entier naturel $n$,
+        $${miseEnEvidence(`0\\leqslant u_n\\leqslant u_{n+1}\\leqslant 1`)}$.`,
+        `La suite est croissante et majorée par $1$ ; elle converge d'après le théorème de convergence monotone. Notons $\\ell$ sa limite. Comme, pour tout entier naturel $n$, $0\\leqslant u_n\\leqslant 1$, le passage à la limite donne
+        \\[0\\leqslant \\ell\\leqslant 1.\\]
+        Ainsi, $\\ell\\in[0\\,;\\,1]$.<br><br>
+        D'après le théorème du point fixe, la suite $(u_n)$ converge vers une valeur $\\ell$ qui vérifie $f(\\ell)=\\ell$.<br><br>
+        On résout l'équation $f(x)=x$ :
+        \\[\\begin{aligned}
+        f(x)=x
+        &\\iff ${p}x^2-${2 * p}x+${p}=0\\\\
+        &\\iff ${p}(x-1)^2=0\\\\
+        &\\iff x=1.
+        \\end{aligned}\\]<br><br>
+        Ainsi, $${miseEnEvidence(`(u_n)\\text{ converge vers }1`)}$.`,
+        `La fonction renvoie le plus petit rang $n$ tel que $u_n\\geqslant1-${hTex}=${seuil}$.<br><br>
+        À la calculatrice, en utilisant le mode « Suite » ou le module « Python », on vérifie que $u_{${rang - 1}}<${seuil}$ et $u_{${rang}}\\geqslant${seuil}$.<br><br>
+        On a donc atteint le seuil $${seuil}$ pour la première fois au rang $${rang}$.<br><br>
+        La valeur de $n$ renvoyée est $${miseEnEvidence(texNombre(rang))}$.`,
+        createList({
+          style: 'alpha',
+          items: [
+            `On a $u_0=0$, puis :
+            \\[\\begin{aligned}
+            u_1
+            &=\\dfrac{${a}\\times 0-${p}}{${p}\\times 0-${p + 1}}\\\\
+            &=\\dfrac{${p}}{${p + 1}},\\\\[1em]
+            u_2
+            &=\\dfrac{${a}\\times\\dfrac{${p}}{${p + 1}}-${p}}{${p}\\times\\dfrac{${p}}{${p + 1}}-${p + 1}}\\\\
+            &=\\dfrac{\\dfrac{-${2 * p}}{${p + 1}}}{\\dfrac{-${2 * p + 1}}{${p + 1}}}\\\\
+            &=\\dfrac{${2 * p}}{${2 * p + 1}},\\\\[1em]
+            u_3
+            &=\\dfrac{${a}\\times\\dfrac{${2 * p}}{${2 * p + 1}}-${p}}{${p}\\times\\dfrac{${2 * p}}{${2 * p + 1}}-${p + 1}}\\\\
+            &=\\dfrac{\\dfrac{-${3 * p}}{${2 * p + 1}}}{\\dfrac{-${3 * p + 1}}{${2 * p + 1}}}\\\\
+            &=\\dfrac{${3 * p}}{${3 * p + 1}}.
+            \\end{aligned}\\]
+            Ainsi, $${miseEnEvidence(termes.join(',\\quad '))}$.`,
+            `On conjecture que, pour tout entier naturel $n$,
+            \\[u_n=${general}.\\]
+            Démontrons cette conjecture par récurrence.<br><br>
+            Pour tout entier naturel $n$, on note $\\mathcal P_n$ la propriété :
+            \\[u_n=${general}.\\]<br>
+            ${texteEnCouleurEtGras('Initialisation :', 'black')}<br><br>
+            Pour $n=0$, on a
+            \\[\\dfrac{${p}\\times 0}{${p}\\times 0+1}=0=u_0.\\]
+            La propriété $\\mathcal P_0$ est donc vraie.<br><br>
+            ${texteEnCouleurEtGras('Hérédité :', 'black')}<br><br>
+            Soit $n\\in\\mathbb N$. Supposons que la propriété $\\mathcal P_n$ est vraie, c'est-à-dire :
+            \\[u_n=${general}.\\]
+            Montrons que la propriété $\\mathcal P_{n+1}$ est vraie, c'est-à-dire :
+            \\[u_{n+1}=\\dfrac{${p}(n+1)}{${p}(n+1)+1}.\\]
+            D'après la relation de récurrence et l'hypothèse de récurrence,
+            \\[\\begin{aligned}
+            u_{n+1}
+            &=\\dfrac{${numerateurSuite}}{${denominateurSuite}}\\\\
+            &=\\dfrac{${a}\\times\\dfrac{${p}n}{${p}n+1}-${p}}{${p}\\times\\dfrac{${p}n}{${p}n+1}-${p + 1}}\\\\
+            &=\\dfrac{\\dfrac{-${p}(n+1)}{${p}n+1}}{\\dfrac{-(${p}n+${p + 1})}{${p}n+1}}\\\\
+            &=\\dfrac{${p}(n+1)}{${p}n+${p + 1}}\\\\
+            &=\\dfrac{${p}(n+1)}{${p}(n+1)+1}.
+            \\end{aligned}\\]
+            La propriété $\\mathcal P_{n+1}$ est donc vraie. La propriété est héréditaire.<br><br>
+            ${texteEnCouleurEtGras('Conclusion :', 'black')}<br><br>
+            La propriété est vraie au rang $0$ et elle est héréditaire. Donc, par récurrence, pour tout entier naturel $n$, $${miseEnEvidence(`u_n=${general}`)}$.`,
+          ],
+        }),
+      ],
+    })
+    const correction = `${partieA}<br><br>${correctionsPartieA}<br>${partieB}<br><br>${correctionsPartieB}`
+
+    this.listeQuestions.push(enonce)
+    this.listeCorrections.push(correction)
   }
 }
